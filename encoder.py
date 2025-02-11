d32f4b5cfc5181d733267de290bd1f01cb78b2d0
ash2d <danielmartinash@gmail.com>
2025-02-11 17:18:31 +0000

    encoder file and python creator

--- encoder.py
+++ encoder.py
@@ -1,14 +1,42 @@
 import torch
+import os
 from iecdt_lab.autoencoder import CNNAutoencoder
+from omegaconf import DictConfig, OmegaConf
+import hydra
+import torchvision
+
 
 '''code to read in the trained ae from a file, remove the decoder.
 this function will then recieve a tile and output the embedding'''
+@hydra.main(version_base=None, config_path="config_ae", config_name="train")
+def remove_decoder(cfg: DictConfig):
+    '''model_path is the path to the trained autoencoder model
+    returns the encoder model class.'''
+    model = CNNAutoencoder(latent_dim = cfg.latent_dim)
+    checkpoint = torch.load(model_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    
+    model = model.to(cfg.device)
 
-def load_encoder(model_path):
-    autoencoder = CNNAutoencoder()
-    autoencoder.load_state_dict(torch.load(model_path))
-    encoder = autoencoder.encoder
+    encoder = model.encoder
+    encoder.eval()
+    torch.save(encoder.state_dict(), '/home/users/dash/iecdt_bjd_cloud_behaviour/encoder.pth')
+    return
+
+@hydra.main(version_base=None, config_path="config_ae", config_name="train")
+def load_encoder(cfg: DictConfig):
+    '''model_path is the path to the trained autoencoder model
+    returns the encoder model class.'''
+    model = CNNAutoencoder(latent_dim = cfg.latent_dim)
+    encoder = model.encoder
+    encoder.load_state_dict(torch.load(encoder_path))
     encoder.eval()
     return encoder
 
-#model_path = "model.pth"
+# model.load_state_dict(torch.load("path/to/experiment/model.pth"))
+
+path = "/home/users/dash/iecdt_bjd_cloud_behaviour/"
+model_path = os.path.join(path, "model.pth")
+encoder_path = os.path.join(path, "encoder.pth")
+remove_decoder()
+load_encoder()
